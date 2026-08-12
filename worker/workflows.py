c93b58ab4a9baa256c3285b0ce8128d6dc1cca6a
adam-quan <adam.quan@temporal.io>
2026-08-12 09:56:04 -0500

    changed greeting to chinese

--- worker/workflows.py
+++ worker/workflows.py
@@ -24,9 +24,9 @@ with workflow.unsafe.imports_passed_through():
 
 # ---------------------------------------------------------------------------
 # Edit me to produce a new workflow version, then `git push`.
-#GREETING = "你好"
+GREETING = "你好"
 #GREETING = "Hello"
-GREETING = "Bonjour"
+#GREETING = "Bonjour"
 # ---------------------------------------------------------------------------
 
 
